5f431ae54087dfe8e5e8c9312c857cb722dc9a69
Awelani Tshilongamulenzhe <atshilongamulenzhe@ulwembubs.com>
2026-08-26 21:59:34 +0200

    Delete E-Health directory

--- E-Health/E-Health-System-dev-backend-ulwembu/src/main/java/co/ehealth/platform/pharmacy/DispensingRecordRepository.java
+++
@@ -1,8 +0,0 @@
-package co.ehealth.platform.pharmacy;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface DispensingRecordRepository extends JpaRepository<DispensingRecord, UUID> {
-}
