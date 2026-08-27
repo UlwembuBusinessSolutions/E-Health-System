a22f462550b5a547bee2ebbf362638fc589cbeb8
Mpho collen Ndou <mndou@ulwembubs.com>
2026-08-27 09:42:26 +0200

    delete fills

--- src/main/java/co/ehealth/platform/core/security/InvalidTokenException.java
+++
@@ -1,7 +0,0 @@
-package co.ehealth.platform.core.security;
-
-public class InvalidTokenException extends RuntimeException {
-    public InvalidTokenException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
